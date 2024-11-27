2f40e344d6852c2deb1926b441686181a212a1d6
Chavdar Momchilov <chavmom@gmail>
2024-11-27 10:05:23 +0200

    Changes to be committed: 	modified:   src/components/data-binding/data-binding.component.html 	modified:   src/components/data-binding/data-binding.component.ts

--- src/components/data-binding/data-binding.component.ts
+++ src/components/data-binding/data-binding.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
+  firstName:string = "Chavdar";
+  rollNo: number = 121;
+  isActive: boolean = true;
+  currentDate: Date = new Date();
+  myPlaceholder: string = "Enter Full Name";
+  div1ClassName: string = "bg-primary";
+  selectedCity: string = "";
+
+  constructor() {
+    console.log(this.firstName);
+    //this.showWelcomeMessage();
+
+    this.isActive = false;
+    console.log(this.isActive);
+  }
+
+  showWelcomeMessage(){
+    alert("Welcome to Angular 19 Tutorials.")
+  }
+
+  onCityChanges(){
+    console.log("City Changed.")
+  }
 
 }
